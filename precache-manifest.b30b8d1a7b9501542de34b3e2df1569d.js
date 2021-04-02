c7f4a05d55f622c485e96cd25ec180fb34a6e17e
gothaw <rad.soltan@gmail.com>
2021-04-02 12:43:08 +0100

    Updates

---
+++ precache-manifest.b30b8d1a7b9501542de34b3e2df1569d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "132a1b6bce317d92f1b3",
+    "url": "/deploy-app-tth/static/js/runtime~main.132a1b6b.js"
+  },
+  {
+    "revision": "0a29996f54f6229b928c",
+    "url": "/deploy-app-tth/static/js/main.0a29996f.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/deploy-app-tth/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "0a29996f54f6229b928c",
+    "url": "/deploy-app-tth/static/css/main.3bc0c2fb.chunk.css"
+  },
+  {
+    "revision": "239511a130f39381b378c8d9176b7875",
+    "url": "/deploy-app-tth/index.html"
+  }
+];
